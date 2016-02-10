2792a4346058afeb66f5b863414fa6ade1ab9c85
Kevin Duong <kdy480@yahoo.com>
2016-02-09 20:16:31 -0500

    starter project

--- Yelp/AppDelegate.swift
+++ Yelp/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Yelp
 //
-//  Created by Kevin Duong on 2/9/16.
-//  Copyright © 2016 Kevin Duong. All rights reserved.
+//  Created by Timothy Lee on 9/19/14.
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
 //
 
 import UIKit
